4460c9f8bb6f96d2b03eed7554b7c531ac9ff031
Carsten Götzinger <info@cgo-it.de>
2018-02-15 07:08:22 +0100

    send response in ajax Controller/AjaxRateItController.php

--- src/Controller/AjaxRateItController.php
+++ src/Controller/AjaxRateItController.php
@@ -10,7 +10,7 @@ namespace cgoIT\rateit\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use cgoIT\rateit\RateIt;
 
 class AjaxRateItController extends Controller {
@@ -29,6 +29,8 @@ class AjaxRateItController extends Controller {
 
         $response = $controller->doVote();
         $response->send();
+
+        return new Response(null);
     }
 
 }
