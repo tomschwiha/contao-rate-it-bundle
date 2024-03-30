8b357270346fc50a831b8d2b712ff80eba462017
Tom Schwiha <tom@schwiha.de>
2024-03-30 17:22:26 +0100

    Update tl_news.php

--- src/Resources/contao/dca/tl_news.php
+++ src/Resources/contao/dca/tl_news.php
@@ -41,7 +41,7 @@ $GLOBALS['TL_DCA']['tl_news']['config']['ondelete_callback'][] = array('tl_news_
  * Palettes
  */
 $GLOBALS['TL_DCA']['tl_news']['palettes']['__selector__'][] = 'addRating';
-$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = $GLOBALS['TL_DCA']['tl_news']['palettes']['default'].';{rating_legend:hide},addRating';
+$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = ($GLOBALS['TL_DCA']['tl_news']['palettes']['default'] ?? null).';{rating_legend:hide},addRating';
 
 /**
  * Add subpalettes to tl_article
