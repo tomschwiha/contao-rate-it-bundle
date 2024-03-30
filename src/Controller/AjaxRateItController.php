5979f7c80b037eb3245544875575e0d1aee6ddae
Tom Schwiha <tom@schwiha.de>
2024-03-30 17:07:47 +0100

    Update AjaxRateItController.php

--- src/Controller/AjaxRateItController.php
+++ src/Controller/AjaxRateItController.php
@@ -9,11 +9,11 @@
 namespace cgoIT\rateit\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use cgoIT\rateit\RateIt;
 
-class AjaxRateItController extends Controller {
+class AjaxRateItController extends AbstractController {
     /**
      * Handles rating requests.
      *
